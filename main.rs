336f5c56f49c90f83c55912895d860b4b1a85f38
Cr-Browning <152670026+Cr-Browning@users.noreply.github.com>
2024-11-25 11:53:06 -0500

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,12 @@
+mod game;
+mod utils;
+use game::*;
+
+fn main() {
+    let fen_str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    let game = Game::read_FEN(fen_str);
+   // let (first_row, rest) = split_on(fen_str, '/');
+    //println!("First: {}, Second: {}", first_row, rest);
+    println!("{}", game.to_string());
+    println!("{:?} {:?} {}", game.active_color, game.en_passant, game.fullmove_number);
+}
